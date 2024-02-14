a57fe77eb8c2431c12702485fd4141afb79c21ed
harryh38 <harryhing2@gmail.com>
2024-02-14 20:03:01 +0000

    description of charts updated

--- src/components/pages/CityIndividual.jsx
+++ src/components/pages/CityIndividual.jsx
@@ -12,20 +12,14 @@ function CityIndividual() {
       <section className="section--container" id="cityIndividual">
         <SearchResult cityName={cityName} />
         <CityMap cityName={cityName} />
+
         <div className="card_desc--wrapper">
-          1 Some informationSome informationSome informationSome informationSome
-          informationSome informationSome informationSome information
-        </div>
-        <div className="card_image--wrapper">
-          <img src="/images/sample/img_card02.png" alt="" />
-        </div>
-        <div className="card_desc--wrapper">
-          2 Some informationSome informationSome informationSome informationSome
-          informationSome informationSome informationSome information
-        </div>
-        <div className="card_desc--wrapper">
-          2 Some informationSome informationSome informationSome informationSome
-          informationSome informationSome informationSome information
+          Our chart visually represents comprehensive air pollution data for the
+          selected country. It provides insights into various pollutants such as
+          carbon monoxide (CO), ozone (O3) and others, offering a clear snapshot
+          of the air quality situation. Users can easily grasp the levels of
+          different pollutants, aiding in understanding environmental conditions
+          and fostering informed decision-making for health and well-being
         </div>
         <Harrycomponent />
       </section>
